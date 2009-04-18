c5edc336caff5c4a7241a458d81e10cec043fb87
taipanromania <taipanromania@localhost>
2009-04-18 15:38:22 +0000

    typo

--- lib/propertydelegate.cpp
+++ lib/propertydelegate.cpp
@@ -14,11 +14,11 @@
  **                  GNU Lesser General Public License                    **
  *                                                                         *
  *   This library is free software: you can redistribute it and/or modify  *
- *   it under the terms of the GNU Lesser General Public License as        * 
+ *   it under the terms of the GNU Lesser General Public License as        *
  *   published by the Free Software Foundation, either version 3 of the    *
  *   License, or (at your option) any later version.                       *
  *   You should have received a copy of the GNU Lesser General Public      *
- *   License along with this library.                                      * 
+ *   License along with this library.                                      *
  *   If not, see <http://www.gnu.org/licenses/>.                           *
  *                                                                         *
  *   This library is distributed in the hope that it will be useful,       *
